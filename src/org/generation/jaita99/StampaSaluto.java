a9b52bf699a26eb79bed3f34dcf1852de38d772f
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-11-28 19:07:39 +0100

    stampato il saluto

--- src/org/generation/jaita99/StampaSaluto.java
+++ src/org/generation/jaita99/StampaSaluto.java
@@ -1,2 +1,7 @@
-package org.generation.jaita99;public class StampaSaluto {
+package org.generation.jaita99;
+
+public class StampaSaluto {
+    public static void main(String[] args) {
+        System.out.println("ciao");
+    }
 }
